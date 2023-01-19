220c439d1f90b89baf85e1bea8a43c37a0e83107
firefly2442 <firefly2442@gmail.com>
2023-01-19 17:42:25 -0600

    pull from local registry first, addresses #1

--- main.py
+++ main.py
@@ -18,29 +18,16 @@ def run():
     while True:
         client = docker.from_env()
 
+        # pull local registry items first so we save on bandwidth by hopefully having a cache-hit when we pull the external tag in a moment
         for img in client.images.list(all=True):
             for tag in img.tags:
-                logging.info(f"Pulling: {tag}")
-                try:
-                    res = client.images.pull(tag)
-                    logging.info(f"Successfully pulled: {res}")
-                except Exception as error:
-                    logging.error(error)
-                
-                if os.environ.get('DOCKER_REGISTRY'):
-                    if not tag.startswith(os.environ.get('DOCKER_REGISTRY')):
-                        try:
-                            tstatus = img.tag(os.environ.get('DOCKER_REGISTRY') + tag)
-                            if tstatus:
-                                logging.info(f"Tagging: {os.environ.get('DOCKER_REGISTRY') + tag}")
-                            else:
-                                logging.error(f"Unable to set tag: {os.environ.get('DOCKER_REGISTRY') + tag}")
-                            resp = client.images.push(os.environ.get('DOCKER_REGISTRY') + tag, stream=True, decode=True)
-                            logging.info(f"Pushed {os.environ.get('DOCKER_REGISTRY') + tag}, received response: {resp}")
-                        except Exception as error:
-                            logging.error(error)
-                else:
-                    logging.warning("DOCKER_REGISTRY is not set")
+                if tag.startswith(os.environ.get('DOCKER_REGISTRY')):
+                    update_tag(tag, client, img, logging)
+        
+        # then go through and pull all (including external registries)
+        for img in client.images.list(all=True):
+            for tag in img.tags:
+                update_tag(tag, client, img, logging)
 
         try:
             if os.environ.get('DOCKER_PRUNE') and os.environ.get('DOCKER_PRUNE').lower() == "true":
@@ -54,6 +41,29 @@ def run():
         logging.info("Sleeping for one hour")
         time.sleep(60 * 60)
         
+
+def update_tag(tag, client, img, logging):
+    logging.info(f"Pulling: {tag}")
+    try:
+        res = client.images.pull(tag)
+        logging.info(f"Successfully pulled: {res}")
+    except Exception as error:
+        logging.error(error)
+    
+    if os.environ.get('DOCKER_REGISTRY'):
+        if not tag.startswith(os.environ.get('DOCKER_REGISTRY')):
+            try:
+                tstatus = img.tag(os.environ.get('DOCKER_REGISTRY') + tag)
+                if tstatus:
+                    logging.info(f"Tagging: {os.environ.get('DOCKER_REGISTRY') + tag}")
+                else:
+                    logging.error(f"Unable to set tag: {os.environ.get('DOCKER_REGISTRY') + tag}")
+                resp = client.images.push(os.environ.get('DOCKER_REGISTRY') + tag, stream=True, decode=True)
+                logging.info(f"Pushed {os.environ.get('DOCKER_REGISTRY') + tag}, received response: {resp}")
+            except Exception as error:
+                logging.error(error)
+    else:
+        logging.warning("DOCKER_REGISTRY is not set")
 
 if __name__ == '__main__':
     run()
